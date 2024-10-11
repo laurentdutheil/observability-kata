192bbda0814828a9ff062858088478272ad904de
Laurent Dutheil <ldutheil74@gmail.com>
2024-10-11 17:11:49 +0200

    refactor test

--- acceptance_test/todo_test.go
+++ acceptance_test/todo_test.go
@@ -11,23 +11,36 @@ import (
 )
 
 func TestAddValidTodo(t *testing.T) {
+	bodyPost := createValidTodo()
+	server := rest.NewApiServer()
+
+	response := postTodoCreation(server, bodyPost)
+
+	assert.Equal(t, http.StatusCreated, response.Code)
+	m := parseBodyResponse(response)
+	assert.NotNil(t, m["id"])
+	assert.Equal(t, "New Todo", m["title"])
+	assert.Equal(t, "Description of the todo", m["description"])
+}
+
+func parseBodyResponse(response *httptest.ResponseRecorder) map[string]interface{} {
+	var m map[string]interface{}
+	_ = json.NewDecoder(response.Body).Decode(&m)
+	return m
+}
+
+func postTodoCreation(server *rest.ApiServer, bodyPost []byte) *httptest.ResponseRecorder {
+	request, _ := http.NewRequest(http.MethodPost, "/todo", bytes.NewBuffer(bodyPost))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, request)
+	return response
+}
+
+func createValidTodo() []byte {
 	bodyPost := []byte(`{
 		"title": "New Todo",
 		"description": "Description of the todo"
 	}`)
-
-	request, _ := http.NewRequest(http.MethodPost, "/todo", bytes.NewBuffer(bodyPost))
-	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	response := httptest.NewRecorder()
-
-	server := rest.NewApiServer()
-	server.ServeHTTP(response, request)
-
-	assert.Equal(t, http.StatusCreated, response.Code)
-
-	var m map[string]interface{}
-	_ = json.NewDecoder(response.Body).Decode(&m)
-	assert.NotNil(t, m["id"])
-	assert.Equal(t, "New Todo", m["title"])
-	assert.Equal(t, "Description of the todo", m["description"])
+	return bodyPost
 }
